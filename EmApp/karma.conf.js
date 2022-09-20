ba86f713451d66c00df072dc8eb59ac209f44919
apsakti <agung.prasetyo@ti.ukdw.ac.id>
2022-09-21 05:34:21 +0700

    replace employeeManagement
    replace employeeManagement to emApp

--- EmApp/karma.conf.js
+++ EmApp/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/employee-management'),
+      dir: require('path').join(__dirname, './coverage/em-App'),
       subdir: '.',
       reporters: [
         { type: 'html' },
